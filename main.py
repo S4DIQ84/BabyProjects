08762b5f99d70a2ac5867354f402a5c5ca1ba3dd
C.P. Mohammed Sadiq Ali <119723238+S4DIQ84@users.noreply.github.com>
2024-02-06 23:16:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+with open("File_name","r") as names:
+    name_list = names.readlines()
+
+with open("letter_name","r") as letter:
+    letter_content = letter.read()
+
+for name in name_list:
+    new_contents = letter_content.replace("[name]", name.strip())
+    with open(f'file_name{name.strip()}.txt', 'w') as files:
+        files.write(new_contents)
+
